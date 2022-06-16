ed64fe07ebde298652595af12bdfce3236f3367f
luseno-hub <luseno@outlook.com>
2022-06-16 14:48:59 +0300

    button styles

---
+++ src/components/styles/Button.style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  font-size: 1rem;
+  padding: 1rem 2rem;
+  background-color: #eb1363;
+  border-radius: 0.5rem;
+  border: none;
+  color: #fff;
+  cursor: pointer;
+  font-weight: bold;
+  text-transform: capitalize;
+`;
