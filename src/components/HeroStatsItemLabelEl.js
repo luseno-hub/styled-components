4b240c98ae1c5478161e8a0e4266d6a5e0d2121b
luseno-hub <luseno@outlook.com>
2022-06-16 14:48:35 +0300

    destructured props

---
+++ src/components/HeroStatsItemLabelEl.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { HeroStatsItemLabel } from "./styles/Hero.style";
+
+function HeroStatsItemLabelEl({ content }) {
+  return <HeroStatsItemLabel>{content}</HeroStatsItemLabel>;
+}
+
+export default HeroStatsItemLabelEl;
---
+++ src/components/HeroStatsItemStatEl.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { HeroStatsItemStat } from "./styles/Hero.style";
+
+function HeroStatsItemStatEl({ content }) {
+  return <HeroStatsItemStat>{content}</HeroStatsItemStat>;
+}
+
+export default HeroStatsItemStatEl;
